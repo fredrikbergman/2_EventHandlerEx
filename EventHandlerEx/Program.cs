4302d79a754f390d6a96b89bc3872df1e59da6b0
Fredrik B <fredrik.bergman@semcon.com>
2018-01-30 08:22:21 +0100

    Add project files.

---
+++ EventHandlerEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHandlerEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
